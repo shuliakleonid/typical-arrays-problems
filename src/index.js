859558d54d79a15098293b5118306f6b6b9e1a21
Leonid Shuliak <zeleny777@gmail.com>
2020-09-29 19:36:20 +0300

    init: Start Typical Arrays Problems-task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,30 @@
-
 exports.min = function min (array) {
-  return 0;
+                if( array == undefined){
+        return 0;
+    }else if(array.length == 0){
+     return 0;
+    }else  {
+       return Math.min.apply(null,array);
+    }
 }
+  
+  exports.max = function max (array) {
+        if(array == undefined){
+            return 0;
+        }else if(array == 0){
+            return 0;
+        }else {
+            return Math.max.apply(null,array);
+        }
+  }
 
-exports.max = function max (array) {
-  return 0;
-}
-
-exports.avg = function avg (array) {
-  return 0;
-}
+    exports.avg = function avg (array) {
+    if(array == undefined){
+        return 0;
+    }else if(array == 0){
+        return 0;
+    }else { 
+        return array.reduce((a, b) => (a + b)) / array.length;
+           }
+  }
+  
